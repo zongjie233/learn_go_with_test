5598926765997b5e64052e91ff92a6f402d7cd8e
zongjie233 <840573195@qq.com>
2023-03-20 22:03:16 +0800

    完成指针与错误章节

--- src/v6Pointer/bankSystem.go
+++ src/v6Pointer/bankSystem.go
@@ -1,6 +1,12 @@
 package v6Pointer
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+// error变量化
+var InsufficientFundsError = errors.New("cannot withdraw, insufficient funds")
 
 type Bitcoin int
 
@@ -21,8 +27,14 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+
+		// errors.New() 创建一个新的error，并带有选择的消息
+		return InsufficientFundsError
+	}
 	w.balance -= amount
+	return nil
 }
 
 // Balance不使用指针无法对数据进行更改
@@ -30,3 +42,15 @@ func (w *Wallet) Withdraw(amount Bitcoin) {
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
+
+/*
+总结：
+如果写的函数需要改变状态，则必须用指针指向需要修改的值
+指针可以是nil，当返回指针时，需要确保其不为nil，避免异常
+
+检查字符串可能会导致测试不稳定，因此可以用一个变量进行代替，更容易测试代码
+
+// errors.New() 创建一个新的error，并带有选择的消息
+// t.Fatal(), 如果被调用将停止测试
+
+*/
--- src/v6Pointer/bankSystem_test.go
+++ src/v6Pointer/bankSystem_test.go
@@ -12,6 +12,17 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
+	assertError := func(t *testing.T, got error, want error) {
+		if got == nil {
+			// Fatal(), 如果被调用将停止测试
+			t.Fatal("didnt get an error but wanted one")
+		}
+
+		if got != want {
+			t.Errorf("got '%s', want'%s'", got, want)
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(10)
@@ -19,11 +30,19 @@ func TestWallet(t *testing.T) {
 
 	})
 
-	t.Run("Withdraw", func(t *testing.T) {
+	t.Run("Withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(10)
 		assertBalance(t, wallet, Bitcoin(10))
 
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, Bitcoin(20))
+		assertError(t, err, InsufficientFundsError)
 	})
 
 }
