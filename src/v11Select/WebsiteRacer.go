18e83287ce5b4dc47e9f94d4b6033fb5eed7df45
zongjie233 <840573195@qq.com>
2023-03-25 22:19:08 +0800

    3.25 select使用，并发使用

--- src/v11Select/WebsiteRacer.go
+++ src/v11Select/WebsiteRacer.go
@@ -5,16 +5,17 @@ import (
 	"time"
 )
 
-func Racer(a, b string) (winner string) {
-	startA := time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
+//func Racer(a, b string) (winner string) {
+//	aDuration := measureResponseTime(a)
+//	bDuration := measureResponseTime(b)
+//	if aDuration < bDuration {
+//		return a
+//	}
+//	return b
+//}
 
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
-	if aDuration < bDuration {
-		return a
-	}
-	return b
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
 }
